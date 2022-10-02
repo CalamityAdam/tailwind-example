704ac9e02709b355137eb95a8111b8250eade590
Adam Sisk <adamsisk91@gmail.com>
2022-10-02 12:07:55 -0500

    adds base tailwind config

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="container">
-      <h1>hello, world</h1>
+    <div className='w-3/4 h-3/4 mx-auto p-12'>
+      <h1 className='text-teal-300 text-3xl'>hello, world</h1>
       <p>welcome to react</p>
     </div>
   );
